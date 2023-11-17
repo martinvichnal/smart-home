d757a8cd970b5b336d8491be2a350ec637bbd332
vmazser <vmazsit@gmail.com>
2023-11-17 21:19:20 +0100

    Update next.config.js

--- smart-home-app/next.config.js
+++ smart-home-app/next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    experimental: {
+        serverActions: true,
+    },
+}
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+module.exports = {
+    // reactStrictMode: true,
+    env: {
+        NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+        NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+        GITHUB_ID: process.env.GITHUB_ID,
+        GITHUB_SECRET: process.env.GITHUB_SECRET,
+    },
+}
