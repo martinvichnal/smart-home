26b3d7c1a4948ddd9f9efeef9148353af475ee21
vmazser <vmazsit@gmail.com>
2023-11-18 12:11:27 +0100

    Adding example code for main.cpp

--- smart-home-firmware/src/main.cpp
+++ smart-home-firmware/src/main.cpp
@@ -1,19 +1,41 @@
 #include <Arduino.h>
 #include <SmartHome.h>
 
-// put function declarations here:
-int myFunction(int, int);
+void setup()
+{
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  // Creating a new smarthome object
+  char[] deviceName = "Bedroom switch";
+  char[] deviceID = "d7b072ca90c48abdb9031436db9dbf54";
+  SmartHome smartHome(deviceName, deviceID);
+
+  // Setting server and WiFi credentials
+  char[] wifiSSID = "ssid";
+  char[] wifiPassword = "password";
+  char[] serverName = "server";
+  char[] serverLink = "serverLink";
+  int serverPort = 80;
+
+  smarthome.setServer(serverLink, serverPort);
+  smarthome.setCredentials("username", "password");
+  smartHome.setWifi(wifiSSID, wifiPassword);
+
+  // Setting Variables along with their types and limits
+  // Types: n - number, b - boolean
+  // Limits: for numbers - min, max, for boolean - none
+  smartHome.addVariableNumber("temperature", n, 0, 100);
+  smartHome.addVariableNumber("humidity", n, 0, 100);
+  smartHome.addVariableBool("light", b);
+  smartHome.addVariableBool("led", b);
+
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+void loop()
+{
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  // DO STUFF HERE
+
+  // Update the smarthome variables and sending to server
+  smartHome.update();
+  smartHome.push();
 }
