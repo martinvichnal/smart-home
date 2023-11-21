2002ce6a6af2686491ff53d02348d59dc4b7c276
vmazser <vmazsit@gmail.com>
2023-11-21 20:57:15 +0100

    v0.2.0.3
    Minor Fix for deployment

--- smart-home-app/next.config.js
+++ smart-home-app/next.config.js
@@ -10,6 +10,7 @@ const nextConfig = {
 module.exports = {
     // reactStrictMode: true,
     env: {
+        DB_CONNECTION_STRING: process.env.DB_CONNECTION_STRING,
         API_KEY: process.env.API_KEY,
         AUTH_DOMAIN: process.env.AUTH_DOMAIN,
         PROJECT_ID: process.env.PROJECT_ID,
