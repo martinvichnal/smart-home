11375ef20f9a8160ed19a9e8355e39cf0bcd7a65
Martin Vichnal <vichnalmartin@gmail.com>
2023-12-10 21:54:12 +0100

    v1.3.1.4

--- smart-home-app/src/app/page.js
+++ smart-home-app/src/app/page.js
@@ -6,8 +6,8 @@ export default function Home() {
             </h1>
             <p className="mt-4 text-center">
                 This is a demo app for my smart home system project. Click on
-                "Devices" on the menu or go to .../devices to see the devices.
-                You have te logged in to see the devices, otherwise you will be
+                Devices on the menu or go to devices to see the devices. You
+                have te logged in to see the devices, otherwise you will be
                 redirected to the login page.
             </p>
         </div>
